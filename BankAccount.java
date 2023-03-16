a272f5747dcbae27a6b0525f03f90756cd0572c4
alex-s4 <al39726867@gmail.com>
2023-03-16 09:14:31 +0800

    Create a getter method for the user's checking and saving account balance.

--- BankAccount.java
+++ BankAccount.java
@@ -3,16 +3,26 @@ package bankAccountAssignment;
 public class BankAccount{
     
     // The class should have the following attributes: (double) checking balance, (double) savings balance.
-    private double checkingBalance;
-    private double savingsBalance;
+    private double checkingBalance = 0;
+    private double savingsBalance = 0;
 
     // Create a class member (static) that has the number of accounts created thus far.
     public static int accountsCreated = 0;
 
     // Create a class member (static) that tracks the total amount of money stored in every account created.
-    static double totalMoneyStored;
+    static double totalMoneyStored = 0;
 
+    // In the constructor, be sure to increment the account count.
     public BankAccount(){
         accountsCreated++;
+    }
+
+    // Create a getter method for the user's checking and saving account balance.
+    public double getCheckingBalance(){
+        return checkingBalance;
+    }
+
+    public double getSavingsBalance(){
+        return savingsBalance;
     }
 }

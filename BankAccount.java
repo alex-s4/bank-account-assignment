1e7a48754bc07a21915426eeae983385c8325836
alex-s4 <al39726867@gmail.com>
2023-03-16 09:04:22 +0800

    Create a class member (static) that tracks the total amount of money stored in every account created.

--- BankAccount.java
+++ BankAccount.java
@@ -8,4 +8,7 @@ public class BankAccount{
 
     // Create a class member (static) that has the number of accounts created thus far.
     static int accountsCreated;
+
+    // Create a class member (static) that tracks the total amount of money stored in every account created.
+    static double totalMoneyStored;
 }

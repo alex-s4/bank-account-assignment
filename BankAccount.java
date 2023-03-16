01b4fb99ba17e27faf365f5af83c803fdf9a578b
alex-s4 <al39726867@gmail.com>
2023-03-16 08:57:27 +0800

    The class should have the following attributes: (double) checking balance, (double) savings balance.

--- BankAccount.java
+++ BankAccount.java
@@ -1,7 +1,9 @@
 package bankAccountAssignment;
 
 public class BankAccount{
-    public static void main(String[] args) {
-        
-    }
+    
+    // The class should have the following attributes: (double) checking balance, (double) savings balance.
+
+    double checkingBalance;
+    double savingsBalance;
 }

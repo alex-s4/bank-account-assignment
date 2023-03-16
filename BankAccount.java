fbece09b58c81e84ca3c4eea19f25aa248ee2d6e
alex-s4 <al39726867@gmail.com>
2023-03-16 08:59:16 +0800

    Create a class member (static) that has the number of accounts created thus far.

--- BankAccount.java
+++ BankAccount.java
@@ -3,7 +3,9 @@ package bankAccountAssignment;
 public class BankAccount{
     
     // The class should have the following attributes: (double) checking balance, (double) savings balance.
-
     double checkingBalance;
     double savingsBalance;
+
+    // Create a class member (static) that has the number of accounts created thus far.
+    static int accountsCreated;
 }

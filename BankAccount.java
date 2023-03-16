6bb4ddb4f05b423055307ba41e1b2c69e99cb8bb
alex-s4 <al39726867@gmail.com>
2023-03-16 09:08:08 +0800

    In the constructor, be sure to increment the account count.

--- BankAccount.java
+++ BankAccount.java
@@ -3,12 +3,16 @@ package bankAccountAssignment;
 public class BankAccount{
     
     // The class should have the following attributes: (double) checking balance, (double) savings balance.
-    double checkingBalance;
-    double savingsBalance;
+    private double checkingBalance;
+    private double savingsBalance;
 
     // Create a class member (static) that has the number of accounts created thus far.
-    static int accountsCreated;
+    public static int accountsCreated = 0;
 
     // Create a class member (static) that tracks the total amount of money stored in every account created.
     static double totalMoneyStored;
+
+    public BankAccount(){
+        accountsCreated++;
+    }
 }
